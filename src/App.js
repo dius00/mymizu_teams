7aeeae2c0b8b227ec761421af75d079ef787b733
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-11-11 06:52:42 +0900

    attempted resolution conflict on staging from jose

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import './App.css';
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Landing/Login";
 import Signup from "./components/Landing/Signup";
 import TeamDashboard from "./components/TeamDashboard/TeamDashboard";
 import Leaderboards from "./components/Leaderboards/Leaderboards";
 import PrivateRoute from "./components/PrivateRoute";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./AuthContext";
+// const firebase = require("firebase");
+// const firebaseui = require("firebaseui");
 
 export default function App() {
 	
@@ -26,30 +31,52 @@ export default function App() {
 	const invalidRoute = () => "Uh oh! Looks like that page doesn't exist :(";
 
 	return (
-		<div>
-		<Router>
-					<div className="app">
-							{ !currentUser &&
-								<nav className="main-nav">
-									<Link to="/">Home</Link>
-								</nav>
-							}
-							{ currentUser &&
-								<nav className="main-nav">
-									<Link to="/dashboard">Team Dashboard</Link>
-									<Link to="/leaderboards">Leaderboards</Link>
-									<a href="#!" onClick={ logoutUser }>Logout</a>
-								</nav>
-							}						<Switch>
-							<Route path="/" exact component={ Landing } />
-							<Route path="/login" exact component={ Login } />
-							<Route path="/signup" exact component={ Signup } />
-							<PrivateRoute path="/dashboard" exact component={ TeamDashboard } />
+    <Container
+			className="d-flex align-items-center justify-content-center"
+			style={{ minHeight: "100vh" }}
+		>
+			<div className="w-100" style={{ maxWidth: "400px" }}>
+				<Router>
+					<AuthProvider>
+						<Switch>
+							{/* <PrivateRoute exact path="/" component={Dashboard} /> */}
+							{/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
+							<Route path="/signup" component={Signup} />
+							<Route path="/login" component={Login} />
+              <Route path="/" exact component={ Landing } />
+
+							{/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+              <PrivateRoute path="/dashboard" exact component={ TeamDashboard } />
 							<PrivateRoute path="/leaderboards" exact component={ Leaderboards } />
 							<Route component={ invalidRoute } />
 						</Switch>
-					</div>
+					</AuthProvider>
 				</Router>
-		</div>
+			</div>
+		</Container>
+		// <div>
+		// <Router>
+		// 			<div className="app">
+		// 					{ !currentUser &&
+		// 						<nav className="main-nav">
+		// 							<Link to="/">Home</Link>
+		// 						</nav>
+		// 					}
+		// 					{ currentUser &&
+		// 						<nav className="main-nav">
+		// 							<Link to="/dashboard">Team Dashboard</Link>
+		// 							<Link to="/leaderboards">Leaderboards</Link>
+		// 							<a href="#!" onClick={ logoutUser }>Logout</a>
+		// 						</nav>
+		// 					}						<Switch>
+		// 					<Route path="/" exact component={ Landing } />
+		// 					<Route path="/login" exact component={ Login } />
+		// 					<Route path="/signup" exact component={ Signup } />
+							
+		// 				</Switch>
+		// 			</div>
+		// 		</Router>
+		// </div>
 	);
 	}
+
--- src/components/Landing/Signup.js
+++ src/components/Landing/Signup.js
@@ -1,4 +1,33 @@
+import React, { useState, useRef } from "react";
+import { useAuth } from "../../AuthContext";
+import {useHistory} from "react-dom"
+
 export default function Signup() {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+	const { signup } = useAuth();
+	const history = useHistory;
+
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError("Passwords do not Match");
+		}
+		try {
+			setError("");
+			setLoading(true);
+			await signup(emailRef.current.value, passwordRef.current.value);
+			history.pushState("/");
+		} catch {
+			setError("Failed to create account");
+		}
+		setLoading(false);
+	}
 
     return (
         <div className="signup-container">
