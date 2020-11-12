1a8a28cfa7d6869edeac742faa6b45eb1f2e3462
Kenny Ng <kenny01123@gmail.com>
2020-11-12 14:08:04 +0900

    uploaded logo

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,45 @@
-  const axios = require('axios')
-  // const token = process.env.TOKEN;
+const axios = require('axios')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
 
-  // axios.defaults.baseURL = 'https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api'
-  // axios.defaults.headers = {'Authorization': `bearer ${token}`}
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase!");
+});
 
-
-  const functions = require("firebase-functions");
-  const admin = require("firebase-admin");
-  const seeds = require('./seeds.json')
-  admin.initializeApp();
-
-
-  // // Create and Deploy Your First Cloud Functions
-  // // https://firebase.google.com/docs/functions/write-firebase-functions
-
-
-  exports.helloWorld = functions.https.onRequest((req, res) => {
-    res.send("Hello from Firebase!");
+//check mymizu user
+exports.checkValidUser = functions.https.onRequest(async (req, res) => {
+  const userName = req.query.name;
+  console.log(userName);
+  try{
+  const { data } = await axios({
+    method: 'get',
+    url: `https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/users/byUsername?username=${userName}`,
+    headers: {
+      'Authorization': `Bearer ${functions.config().mymizu.key}`//${functions.config().mymizu.key}`
+    },
   });
+  res.send(true);
+  }catch(error) {
+    res.send(false)
+  }
+});
 
-
-  //check mymizu user
-
-  exports.checkValidUser = functions.https.onRequest(async (req, res) => {
-    const userName = req.query.name;
-    console.log(userName);
-    try{
-    const { data } = await axios({
+//see user refill history 
+exports.checkUserHistory = functions.https.onRequest(async (req, res) => {
+  const userName = req.query.name;
+  try {
+  const { data } = await axios({
       method: 'get',
-      url: `https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/users/byUsername?username=${userName}`,
+      url: `https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/users/byUsername/refills?username=${userName}`,
       headers: {
         'Authorization': `Bearer ${functions.config().mymizu.key}`//${functions.config().mymizu.key}`
       },
-    });
-    res.send(true);
-    }catch(error) {
-      res.send(false)
-    }
-  });
+    })
+    res.send(data.refills);
+  } catch (error) { res.send(error) }
+});
 
 //getSortedTeams 
 exports.sortTeams = functions.https.onRequest(async (req, res) => {
@@ -202,80 +204,21 @@ exports.getUserWeeklyVolume = functions.https.onRequest(async (req, res) => {
       res.send("done");
     });
 
-
-
-  //seeding 
-  exports.seed = functions.https.onRequest((req, res) => {
-    const usersRef = admin.database().ref();
-    admin
-      .database()
-      .ref()
-      .set(seeds);
-    usersRef.once("value", (data) => {
-      res.send(data.val());
-    });
-  });
-
-  //getting data 
-  exports.showData = functions.https.onRequest((req, res) => {
-    const usersRef = admin.database().ref();
-    usersRef.once("value", (data) => {
-      res.send(data.val());
-    });
-  });
-
-  exports.showUsers = functions.https.onRequest((req, res) => {
-    const usersRef = admin.database().ref("users");
-    usersRef.once("value", (data) => {
-      res.send(data.val());
-    });
-  });
-
-  exports.showTeams = functions.https.onRequest((req, res) => {
-    const teamsRef = admin.database().ref("teams")
-    teamsRef.once("value", (data) => {
-      res.send(data.val())
-    });
-  });
-
-
-
-
-
-
-  //post requests
-  exports.addUser = functions.https.onRequest((req, res) => {
-    const usersRef = admin.database().ref("users");
-    const newUserRef = usersRef.push();
-    newUserRef.set({
-      user_name: req.query.name,
-      mm_user_name: req.query.mm_username,
-    });
-    usersRef.once("value", (data) => {
-      res.send(data.val());
-    });
-  });
-
-  exports.addTeam = functions.https.onRequest((req, res) => {
-    const teamsRef = admin.database().ref("teams");
-    const newTeamRef = usersRef.push();
-    newTeamRef.set({
-      team_name: req.query.name,
-      members: req.query.memberList,
-    });
-    teamsRef.once("value", (data) => {
-      res.send(data.val());
-    });
-  });
-
-
-  exports.updateUser = functions.https.onRequest((req, res) => {
-    const usersRef = admin.database().ref("users");
-    const user = usersRef.child(req.query.user_name);
-    user.update({
-      nickname: "Amazing Grace",
-    });
-    user.once("value", (data) => {
-      res.send(data);
-    });
+//get and update team volume 
+exports.updateMonthlyVolume = functions.https.onRequest(async (req, res) => {
+  const teamRef = db.collection('teams');
+  const record = await teamRef.get();
+  record.forEach(async (team) => {
+    let result=0;
+    for (const member of team.data().members) {
+      if(member) {
+        try{
+      // eslint-disable-next-line no-await-in-loop
+      const {data}= await axios.get(`http://localhost:5001/mymizuteams/us-central1/getUserMonthlyVolume?name=${member}`)
+      result+=data;
+    }catch(error) {console.log(error)}
+    }}
+    team.ref.update({monthly_water: result});
+    })
+    res.send("done");
   });
