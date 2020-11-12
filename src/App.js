e9e3569884ffc53af9ee9ac1d2ecfa35191aae27
Jose M. Torres <jmtorresr@gmail.com>
2020-11-12 09:43:04 +0900

    Updated design on Signup Form

--- src/App.js
+++ src/App.js
@@ -1,59 +1,69 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-// import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Landing/Login";
 import Signup from "./components/Landing/Signup";
 import TeamDashboard from "./components/TeamDashboard/TeamDashboard";
 import Leaderboards from "./components/Leaderboards/Leaderboards";
 import PrivateRoute from "./components/PrivateRoute";
-import { Container } from "react-bootstrap";
+
 import { AuthProvider } from "./AuthContext";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 // const firebase = require("firebase");
 // const firebaseui = require("firebaseui");
 
 export default function App() {
-	
 	// TODO: set current user after authentication!
 	const currentUser = true; // just for testing; remove later :)
 
 	const logoutUser = async () => {
-		
 		try {
 			// TODO: logs out currently logged in user
 			// assuming logout works, redirect to landing:
-			window.location = "/";			
-		} catch(error) {
+			window.location = "/";
+		} catch (error) {
 			console.error(error);
 		}
-	}
+	};
 
 	const invalidRoute = () => "Uh oh! Looks like that page doesn't exist :(";
 
 	return (
-    <Container
-			className="d-flex align-items-center justify-content-center"
-			style={{ minHeight: "100vh" }}
-		>
-			<div className="w-100" style={{ maxWidth: "400px" }}>
-				<Router>
-					<AuthProvider>
-						<Switch>
-							{/* <PrivateRoute exact path="/" component={Dashboard} /> */}
-							{/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
-							<Route path="/signup" component={Signup} />
-							<Route path="/login" component={Login} />
-              <Route path="/" exact component={ Landing } />
+		<div id="app">
+			<Container
+				className="d-flex align-items-center justify-content-center"
+				style={{ minHeight: "100vh" }}
+			>
+				<div className="w-100" style={{ maxWidth: "400px" }}>
+					<Router>
+						<AuthProvider>
+							<Switch>
+								{/* <PrivateRoute exact path="/" component={Dashboard} /> */}
+								{/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
+								<Route path="/signup" component={Signup} />
+								<Route path="/login" component={Login} />
+								<Route path="/" exact component={Landing} />
 
-							{/* <Route path="/forgot-password" component={ForgotPassword} /> */}
-              <PrivateRoute path="/dashboard" exact component={ TeamDashboard } />
-							<PrivateRoute path="/leaderboards" exact component={ Leaderboards } />
-							<Route component={ invalidRoute } />
-						</Switch>
-					</AuthProvider>
-				</Router>
-			</div>
-		</Container>
+								{/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+								<PrivateRoute
+									path="/dashboard"
+									exact
+									component={TeamDashboard}
+								/>
+								<PrivateRoute
+									path="/leaderboards"
+									exact
+									component={Leaderboards}
+								/>
+								<Route component={invalidRoute} />
+							</Switch>
+						</AuthProvider>
+					</Router>
+				</div>
+			</Container>
+		</div>
 		// <div>
 		// <Router>
 		// 			<div className="app">
@@ -72,11 +82,10 @@ export default function App() {
 		// 					<Route path="/" exact component={ Landing } />
 		// 					<Route path="/login" exact component={ Login } />
 		// 					<Route path="/signup" exact component={ Signup } />
-							
+
 		// 				</Switch>
 		// 			</div>
 		// 		</Router>
 		// </div>
 	);
-	}
-
+}
--- src/components/Landing/Signup.js
+++ src/components/Landing/Signup.js
@@ -1,18 +1,20 @@
 import React, { useState, useRef } from "react";
 import { useAuth } from "../../AuthContext";
-import {useHistory} from "react-dom"
+import { useHistory } from "react-dom";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 
 export default function Signup() {
 	const emailRef = useRef();
 	const passwordRef = useRef();
 	const passwordConfirmRef = useRef();
+	const username = useRef();
 	const { signup } = useAuth();
 	const history = useHistory;
 
 	const [error, setError] = useState("");
 	const [loading, setLoading] = useState(false);
 
-	async function handleSubmit(e) {
+	async function submitSignup(e) {
 		e.preventDefault();
 
 		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
@@ -29,9 +31,57 @@ export default function Signup() {
 		setLoading(false);
 	}
 
-    return (
-        <div className="signup-container">
-            <h1>Sign up to make your own mymizu team: </h1>
-        </div>
-    )
+	return (
+		<Card>
+			<div className="text-center">
+				<img
+					id="logo"
+					src="https://static1.squarespace.com/static/5d2bebc1fc9ee70001122846/t/5dfc1ac767217d42bdd153a6/1605008796817/"
+				></img>
+				<h2 className="text-center mb-4" id="title">
+					Sign up{" "}
+				</h2>
+				<h6 id="label-desc">Let's save the planet together</h6>
+				{error && <Alert variant="danger">{error}</Alert>}
+				<Form onSubmit={submitSignup} className="text-left">
+					<Form.Group id="form">
+						<Form.Label id="label">mymizu username</Form.Label>
+						<Form.Control
+							type="username"
+							ref={username}
+							placeholder="Enter your mymizu username"
+						/>
+					</Form.Group>
+					<Form.Group id="form">
+						<Form.Label id="label">Email</Form.Label>
+						<Form.Control
+							type="email"
+							ref={emailRef}
+							placeholder="Enter your email"
+						/>
+					</Form.Group>
+					<Form.Group id="form">
+						<Form.Label id="label"> Password</Form.Label>
+						<Form.Control
+							type="password"
+							ref={passwordRef}
+							placeholder="Create a password"
+						/>
+					</Form.Group>
+
+					<Form.Group id="form">
+						<Form.Label id="label"> Confirm your password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Confirm your password"
+							ref={passwordConfirmRef}
+						/>
+					</Form.Group>
+					<div className="text-center" id="button">
+						<Button type="submit">Sign up</Button>
+					</div>
+				</Form>
+			</div>
+		</Card>
+	);
 }
